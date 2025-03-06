1ec12af03bd39be4fb41e7ed05c5d0c6bc031abf
Nguyen Huu Vy <huuvy2108@gmail.com>
2025-03-06 19:52:38 +0700

    Tim so lon nhat a,b

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -2,5 +2,19 @@
 
 void main()
 {
-	printf("Hello World\n");
+	int a,b = 0;
+	printf("Nhap a,b:\n");
+	scanf_s("%d%d", &a, &b);
+	if (a = b)
+	{
+		printf("a = b\n");
+	}
+	else if (a > b)
+	{
+		printf("a > b\n");
+	}
+	else
+		{
+		printf("a < b\n");
+	}
 }
