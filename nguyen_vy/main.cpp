e89c7c64976758e482f692664c0d2d5cf8e5f555
huuvy2108 <huuvy2108@gmail.com>
2025-03-09 22:38:25 +0700

    Xac dinh so am, duong

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,52 +1,17 @@
 #include <stdio.h>
-#include <math.h>
 
 void main()
 {
-	float a,b,c,delta,x1,x2;
-	printf("Nhap so a:\n");
-	scanf_s("%f", &a);
-	printf("Nhap so b:\n");
-	scanf_s("%f", &b);
-	printf("Nhap so c:\n");
-	scanf_s("%f", &c);
-	if (a==0)
-	{
-		if (b == 0)
-		{
-			if (c == 0)
-			{
-				printf("Phuong trinh vo so nghiem\n");
-			}
-			else
-			{
-				printf("Phuong trinh vo nghiem\n");
-			}
-		}
-		else
-		{
-			x1 = -c / b;
-			printf("Phuong trinh co nghiem la: %f\n", x1);
-		}
-	}
-	else
-	{
-		delta = b * b - 4 * a * c;
-		if (delta < 0)
-		{
-			printf("Phuong trinh vo nghiem\n");
-		}
-		else if (delta == 0)
-		{
-			x1 = -b / (2 * a);
-			printf("Phuong trinh co nghiem kep la: %f\n", x1);
-		}
-		else
-		{
-			x1 = (-b + sqrt(delta)) / (2 * a);
-			x2 = (-b - sqrt(delta)) / (2 * a);
-			printf("Phuong trinh co 2 nghiem phan biet la: %f va %f\n", x1, x2);
-		}
-	}
-
+    int num;
+    printf("Nhap so nguyen khac 0: ");
+    scanf_s("%d", &num);
+    if (num > 0) {
+        printf("So %d la so duong.\n", num);
+    }
+    else if (num < 0) {
+        printf("So %d la so am.\n", num);
+    }
+    else {
+        printf("Vui long nhap lai so khac 0.\n");
+    }
 }
