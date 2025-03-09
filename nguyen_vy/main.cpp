c5f684f13365c4dde4201ec6f986ba18502b436d
huuvy2108 <huuvy2108@gmail.com>
2025-03-09 22:32:51 +0700

    Giai phuong trinh bac hai ax2 + bx+c = 0

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,22 +1,52 @@
 #include <stdio.h>
+#include <math.h>
 
 void main()
 {
-	int a, b,c = 0;
+	float a,b,c,delta,x1,x2;
 	printf("Nhap so a:\n");
-	scanf_s("%d", &a);
+	scanf_s("%f", &a);
 	printf("Nhap so b:\n");
-	scanf_s("%d", &b);
+	scanf_s("%f", &b);
 	printf("Nhap so c:\n");
-	scanf_s("%d", &c);
-	int max = a;
-	if (b >= max)
+	scanf_s("%f", &c);
+	if (a==0)
 	{
-		max = a;
+		if (b == 0)
+		{
+			if (c == 0)
+			{
+				printf("Phuong trinh vo so nghiem\n");
+			}
+			else
+			{
+				printf("Phuong trinh vo nghiem\n");
+			}
+		}
+		else
+		{
+			x1 = -c / b;
+			printf("Phuong trinh co nghiem la: %f\n", x1);
+		}
 	}
-	if (c >= max)
+	else
 	{
-		max = c;
+		delta = b * b - 4 * a * c;
+		if (delta < 0)
+		{
+			printf("Phuong trinh vo nghiem\n");
+		}
+		else if (delta == 0)
+		{
+			x1 = -b / (2 * a);
+			printf("Phuong trinh co nghiem kep la: %f\n", x1);
+		}
+		else
+		{
+			x1 = (-b + sqrt(delta)) / (2 * a);
+			x2 = (-b - sqrt(delta)) / (2 * a);
+			printf("Phuong trinh co 2 nghiem phan biet la: %f va %f\n", x1, x2);
+		}
 	}
-		printf("So lon nhat la: %d\n",max);
+
 }
