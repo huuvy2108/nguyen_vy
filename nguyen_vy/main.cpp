ddc0b5b348d3782f3680a26849b425df19352632
huuvy2108 <huuvy2108@gmail.com>
2025-03-09 22:41:40 +0700

    Xac dinh chan le

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -2,16 +2,15 @@
 
 void main()
 {
-    int num;
-    printf("Nhap so nguyen khac 0: ");
-    scanf_s("%d", &num);
-    if (num > 0) {
-        printf("So %d la so duong.\n", num);
+    int n;
+    printf("Nhap so nguyen: ");
+    scanf_s("%d", &n);
+    if (n%2 == 0) 
+    {
+        printf("So nay la so chan.\n");
     }
-    else if (num < 0) {
-        printf("So %d la so am.\n", num);
-    }
-    else {
-        printf("Vui long nhap lai so khac 0.\n");
+    else
+    {
+        printf("So nay la so le.\n");
     }
 }
