3e3f5b622e2ba064430122ce9ad5587eac22a46e
huuvy2108 <huuvy2108@gmail.com>
2025-03-11 22:09:52 +0700

    Nhap so 0 thi dung chuong trinh

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -2,13 +2,25 @@
 
 void main()
 {
-    int n;
-    long long giaithua = 1;
-    printf("Nhap so tu nhien n: ");
-    scanf_s("%d", &n);
-    for (int i = 1; i <= n; i += 1) 
-    {
-        giaithua =giaithua*i;
+    int num, error_count = 0;
+
+    printf("Nhap cac so (nhap 0 de dung, qua 5 lan sai se thoat):\n");
+
+    while (1) {
+        scanf_s("%d", &num);
+        if (num == 0) 
+        {  
+            printf("Da nhap 0, ket thuc chuong trinh!\n");
+            break;
+        }
+        else if (num != 0) 
+        {  
+            error_count++;
+        }
+        if (error_count >= 5)
+        {  
+            printf("Ban da nhap sai qua 5 lan, thoat chuong trinh!\n");
+            break;
+        }
     }
-    printf("%d! = %d\n", n, giaithua);
 }
