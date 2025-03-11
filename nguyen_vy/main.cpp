f5592039f7c34251c079f9da79723c0e5853cd41
huuvy2108 <huuvy2108@gmail.com>
2025-03-11 19:58:43 +0700

    BCC

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -2,46 +2,14 @@
 
 void main()
 {
-	int n = 1, pin, tien, temp = 0;
-	int mk = 1234;
-	while (n < 4) 
+	int i = 0,j = 0;
+	for (i = 2; i <= 10; i++)
 	{
-		switch (n)
+		for (int j = 1; j <= 10; j++)
 		{
-		case 1:
-			printf("Nhap ma pin: ");
-			scanf_s("%d", &pin);
-			if (pin == mk)
-			{
-				n = 2;
-			}
-			else
-			{
-				printf("Sai ma pin\n");
-				temp++;
-				if (temp == 3)
-				{
-					n = 3;
-				}
-			}
-			break;
-		case 2:
-			printf("Nhap so tien can rut\n");
-			scanf_s("%d", &tien);
-			if (tien > 10000)
-			{
-				printf("So tien rut phai nho hon 10000\n");
-			}
-			else
-			{
-				printf("Ban da rut %d VND.\n", tien);
-				n = 4;
-			}
-			break;
-		case 3:
-			printf("Ban da nhap sai qua 3 lan. Thoat chuong trinh\n");
-			n = 4;
-			break;
+			printf("%d x %d = %2d\t", i, j, i * j);
 		}
+		printf("\n");
 	}
+	
 }
