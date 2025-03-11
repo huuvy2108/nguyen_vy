69aa84c7f7a69f5553d8bf4204449acede1e9afe
huuvy2108 <huuvy2108@gmail.com>
2025-03-11 21:35:01 +0700

    Tong so chan

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -5,7 +5,7 @@ void main()
     int n, sum = 0;
     printf("Nhap so tu nhien n: ");
     scanf_s("%d", &n);
-    for (int i = 1; i <= n; i += 1) {
+    for (int i = 2; i <= n; i += 2) {
         sum += i;
     }
     printf("Tong cac so tu 1 den %d la: %d\n", n, sum);
