6699f1d1de52268012cc10f5a6ef88e25cf2c329
huuvy2108 <huuvy2108@gmail.com>
2025-03-11 21:42:24 +0700

    Tinh giai thua so nguyen n

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -2,11 +2,13 @@
 
 void main()
 {
-    int n, sum = 0;
+    int n;
+    long long giaithua = 1;
     printf("Nhap so tu nhien n: ");
     scanf_s("%d", &n);
-    for (int i = 2; i <= n; i += 2) {
-        sum += i;
+    for (int i = 1; i <= n; i += 1) 
+    {
+        giaithua =giaithua*i;
     }
-    printf("Tong cac so tu 1 den %d la: %d\n", n, sum);
+    printf("%d! = %d\n", n, giaithua);
 }
