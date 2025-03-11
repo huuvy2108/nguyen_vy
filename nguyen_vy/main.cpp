a9444fdcc75da05f7a544192584cf78c6fd178bb
huuvy2108 <huuvy2108@gmail.com>
2025-03-11 21:25:37 +0700

    Tra tien ATM

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -2,14 +2,39 @@
 
 void main()
 {
-	int i = 0,j = 0;
-	for (i = 2; i <= 10; i++)
-	{
-		for (int j = 1; j <= 10; j++)
-		{
-			printf("%d x %d = %2d\t", i, j, i * j);
-		}
-		printf("\n");
+	int i = 0, tien;
+	int soto50 = 0;
+	int soto100 = 0;
+	int soto200 = 0;
+	int soto500 = 0;
+	int sotien = 0;
+	printf("Nhap so tien can rut: \n");
+	scanf_s("%d", &tien);
+    while (sotien < tien) 
+    {
+        if (sotien + 50 <= tien) 
+        {
+            soto50++;
+            sotien += 50;
+        }
+        if (sotien + 100 <= tien) 
+        {
+            soto100++;
+            sotien += 100;
+        }
+        if (sotien + 200 <= tien) 
+        {
+            soto200++;
+            sotien += 200;
+        }
+        if (sotien + 500 <= tien) 
+        {
+            soto500++;
+            sotien += 500;
+        }
 	}
-	
+	printf("So to 50: %d\n", soto50);
+	printf("So to 100: %d\n", soto100);
+	printf("So to 200: %d\n", soto200);
+	printf("So to 500: %d\n", soto500);
 }
