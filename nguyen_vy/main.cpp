29d5657fa0b35b9fb3d54fdfff7064c0d1bb305f
huuvy2108 <huuvy2108@gmail.com>
2025-03-13 20:19:52 +0700

    Tim vi tri so lon nhat cua mang

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -4,13 +4,21 @@
 
 void main()
 {
-	int sum = 0;
-	int arr_1[10] = {0};
-	for (int i = 0; i < 10; i++)
+	int SLN = 0;
+	int arr_1[] = {1,2,2,4,4,3,1,3};
+	int size = sizeof(arr_1) / sizeof(arr_1[0]);
+	for (int i = 0; i < size; i++)
 	{
-		printf("Nhap phan tu thu %d: ", i + 1);
-		scanf_s("%d", &arr_1[i]);
-		sum = sum + arr_1[i];
+		if (arr_1[i] > SLN)
+			{
+			SLN = arr_1[i];
+			}
 	}
-	printf("Tong cac phan tu trong mang la: %d", sum);
+	for (int i = 0; i < size; i++)
+	{
+		if (arr_1[i] == SLN)
+		{
+			printf("Vi tri So lon nhat la: %d\n", i);
+		}
+	}
 }  
