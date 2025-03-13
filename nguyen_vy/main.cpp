41913bf35fd60cfc3f608698c5ad919133be3bd3
huuvy2108 <huuvy2108@gmail.com>
2025-03-13 19:55:06 +0700

    Mang

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,93 +1,16 @@
 #include <stdio.h>
+#include <conio.h>
+#include <math.h>
 
 void main()
 {
-    int n;
-    printf("Nhap so nguyen duong (toi da 4 chu so): ");
-    scanf_s("%d", &n);
-    if (n < 0 || n > 9999) {
-        printf("Chi ho tro so tu 0 den 9999.\n");
-    }
-    int hangNghin = n / 1000;
-    int hangTram = (n / 100) % 10;
-    int hangChuc = (n / 10) % 10;
-    int hangDonVi = n % 10;
-
-    int coSo = 0;
-    int digit;  
-
-    digit = hangNghin;
-    if (digit > 0) {
-        switch (digit) {
-        case 1: printf("mot "); break;
-        case 2: printf("hai "); break;
-        case 3: printf("ba "); break;
-        case 4: printf("bon "); break;
-        case 5: printf("nam "); break;
-        case 6: printf("sau "); break;
-        case 7: printf("bay "); break;
-        case 8: printf("tam "); break;
-        case 9: printf("chin "); break;
-        }
-        printf("ngan ");
-        coSo = 1;
-    }
-    digit = hangTram;
-    if (digit > 0) {
-        switch (digit) {
-        case 1: printf("mot "); break;
-        case 2: printf("hai "); break;
-        case 3: printf("ba "); break;
-        case 4: printf("bon "); break;
-        case 5: printf("nam "); break;
-        case 6: printf("sau "); break;
-        case 7: printf("bay "); break;
-        case 8: printf("tam "); break;
-        case 9: printf("chin "); break;
-        }
-        printf("tram ");
-        coSo = 1;
-    }
-    else if (coSo && (hangChuc > 0 || hangDonVi > 0)) {
-        printf("khong tram ");
-    }
-    digit = hangChuc;
-    if (digit > 1) {
-        switch (digit) {
-        case 2: printf("hai "); break;
-        case 3: printf("ba "); break;
-        case 4: printf("bon "); break;
-        case 5: printf("nam "); break;
-        case 6: printf("sau "); break;
-        case 7: printf("bay "); break;
-        case 8: printf("tam "); break;
-        case 9: printf("chin "); break;
-        }
-        printf("muoi ");
-    }
-    else if (digit == 1) {
-        printf("muoi ");
-    }
-    else if (digit == 0 && hangDonVi > 0 && coSo) {
-        printf("le ");
-    }
-    digit = hangDonVi;
-    if (digit > 0) {
-        if (hangChuc == 1 && digit == 5) {
-            printf("lam ");
-        }
-        else {
-            switch (digit) {
-            case 1: printf("mot "); break;
-            case 2: printf("hai "); break;
-            case 3: printf("ba "); break;
-            case 4: printf("bon "); break;
-            case 5: printf("nam "); break;
-            case 6: printf("sau "); break;
-            case 7: printf("bay "); break;
-            case 8: printf("tam "); break;
-            case 9: printf("chin "); break;
-            }
-        }
-    }
+	int sum = 0;
+	int arr_1[9] = {};
+	for (int i = 0; i < 10; i++)
+	{
+		printf("Nhap phan tu thu %d: ", i + 1);
+		scanf_s("%d", &arr_1[i]);
+		sum = sum + arr_1[i];
+	}
+	printf("Tong cac phan tu trong mang la: %d", sum);
 }  
