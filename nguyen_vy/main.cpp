9ef9a8e6ee39149fb4828efd034f95ffc47aaab0
huuvy2108 <huuvy2108@gmail.com>
2025-03-16 02:19:32 +0700

    Bài 8: Sắp xếp tăng dần trong mảng

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -2,14 +2,21 @@
 
 void main()
 {
-    int a, b;
-    printf("Nhap hai so nguyen: \n");
-    scanf_s("%d %d", &a, &b);
+    int arr_1[] = { 5, 2, 9, 1, 7, 6, 8, 3, 4, 0 };
+    int size = sizeof(arr_1) / sizeof(arr_1[0]);
 
-    printf("Truoc khi hoan vi: a = %d, b = %d\n", a, b);
-
-    int temp = a;
-    a = b;
-    b = temp;
-    printf("Sau khi hoan vi: a = %d, b = %d\n", a, b);
-}  
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr_1[j] > arr_1[j + 1]) {
+                int temp = arr_1[j];
+                arr_1[j] = arr_1[j + 1];
+                arr_1[j + 1] = temp;
+            }
+        }
+    }
+    printf("Mang sau khi sap xep tang dan: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr_1[i]);
+    }
+    printf("\n");
+}
