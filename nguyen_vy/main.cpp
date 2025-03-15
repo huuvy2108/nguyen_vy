2c44a35441a449029dd9680687a425905f93ed86
huuvy2108 <huuvy2108@gmail.com>
2025-03-16 01:41:40 +0700

    Bài 4: Thay các số âm trong mảng bằng 0

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -2,21 +2,17 @@
 
 void main()
 {
-	int SLN = 0;
-	int arr_1[] = { 1,2,2,4,4,3,1,3 };
-	int size = sizeof(arr_1) / sizeof(arr_1[0]);
-	for (int i = 0; i < size; i++)
-	{
-		if (arr_1[i] > SLN)
-		{
-			SLN = arr_1[i];
-		}
-	}
-	for (int i = 0; i < size; i++)
-	{
-		if (arr_1[i] == SLN)
-		{
-			printf("Vi tri So lon nhat la: %d\n", i);
-		}
-	}
+    int SLN = 0;
+    int arr_1[] = { 1,2,-2,4,-4,3,1,3 };
+    int size = sizeof(arr_1) / sizeof(arr_1[0]);
+    for (int i = 0; i < size; i++)
+    {
+        if (arr_1[i] < 0) {
+            arr_1[i] = 0;
+        }
+    }
+    printf("Mang sau khi thay the so am bang 0:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr_1[i]);
+    }
 }  
