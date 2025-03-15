0f14c00e304e9217d8f1c8025df3c77eed76f393
huuvy2108 <huuvy2108@gmail.com>
2025-03-16 01:34:00 +0700

    Bài 2: Trung bình cộng các số lẻ trong mảng

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -4,9 +4,18 @@ void main()
 {
     int n;
     int arr_1[] = {0,1,2,3,4,5,6,7,8,9};
-    int sum = 0;
+    int sum = 0, count = 0;
     for (int i = 0; i < sizeof(arr_1)/sizeof(arr_1[0]); i++) {
-        sum += arr_1[i];
+		if (arr_1[i] % 2 == 0) {
+            sum += arr_1[i];
+			count ++;
+		}
     }
-    printf("Tong cac phan ta trong mang: %d\n", sum);
+    if (count > 0) {
+        float average = (float)sum / count;
+        printf("Trung binh cong cac so le trong mang: %.2f\n", average);
+    }
+    else {
+        printf("Khong co so le trong mang.\n");
+    }
 }  
