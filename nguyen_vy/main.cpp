599ccd54bb8c204920f6593a810ea3c280a6ae99
huuvy2108 <huuvy2108@gmail.com>
2025-03-16 01:35:42 +0700

    Bài 3: Các vị trí SLN trong mảng

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -2,20 +2,21 @@
 
 void main()
 {
-    int n;
-    int arr_1[] = {0,1,2,3,4,5,6,7,8,9};
-    int sum = 0, count = 0;
-    for (int i = 0; i < sizeof(arr_1)/sizeof(arr_1[0]); i++) {
-		if (arr_1[i] % 2 == 0) {
-            sum += arr_1[i];
-			count ++;
+	int SLN = 0;
+	int arr_1[] = { 1,2,2,4,4,3,1,3 };
+	int size = sizeof(arr_1) / sizeof(arr_1[0]);
+	for (int i = 0; i < size; i++)
+	{
+		if (arr_1[i] > SLN)
+		{
+			SLN = arr_1[i];
 		}
-    }
-    if (count > 0) {
-        float average = (float)sum / count;
-        printf("Trung binh cong cac so le trong mang: %.2f\n", average);
-    }
-    else {
-        printf("Khong co so le trong mang.\n");
-    }
+	}
+	for (int i = 0; i < size; i++)
+	{
+		if (arr_1[i] == SLN)
+		{
+			printf("Vi tri So lon nhat la: %d\n", i);
+		}
+	}
 }  
