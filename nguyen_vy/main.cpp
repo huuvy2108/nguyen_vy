426a1f70f1499196dd2f24397ea820649aa23d4a
huuvy2108 <huuvy2108@gmail.com>
2025-03-16 01:27:40 +0700

    Bài 1: Tính tổng mảng

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,24 +1,12 @@
 #include <stdio.h>
-#include <conio.h>
-#include <math.h>
 
 void main()
 {
-	int SLN = 0;
-	int arr_1[] = {1,2,2,4,4,3,1,3};
-	int size = sizeof(arr_1) / sizeof(arr_1[0]);
-	for (int i = 0; i < size; i++)
-	{
-		if (arr_1[i] > SLN)
-			{
-			SLN = arr_1[i];
-			}
-	}
-	for (int i = 0; i < size; i++)
-	{
-		if (arr_1[i] == SLN)
-		{
-			printf("Vi tri So lon nhat la: %d\n", i);
-		}
-	}
+    int n;
+    int arr_1[] = {0,1,2,3,4,5,6,7,8,9};
+    int sum = 0;
+    for (int i = 0; i < sizeof(arr_1)/sizeof(arr_1[0]); i++) {
+        sum += arr_1[i];
+    }
+    printf("Tong cac phan ta trong mang: %d\n", sum);
 }  
