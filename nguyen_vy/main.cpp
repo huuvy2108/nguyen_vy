4b0651bbf971c24a37e7d18e789d7ec7b95cfa3c
huuvy2108 <huuvy2108@gmail.com>
2025-03-16 02:11:40 +0700

    Bài 8: Hoán vị 2 số nguyên

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,16 +1,15 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 void main()
 {
-    int SLN = 0;
-    int arr_1[] = { 1,2,3,4,5,6,7,8,9 };
-    int size = sizeof(arr_1) / sizeof(arr_1[0]);
-	int max = arr_1[0];
-    for (int i = 0; i < size; i++)
-    {
-        if (arr_1[i] > max) {
-            max = arr_1[i];
-		}
-    }
-    printf("so lon nhat trong mang: %d\n",max);
+    int a, b;
+    printf("Nhap hai so nguyen: \n");
+    scanf_s("%d %d", &a, &b);
+
+    printf("Truoc khi hoan vi: a = %d, b = %d\n", a, b);
+
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("Sau khi hoan vi: a = %d, b = %d\n", a, b);
 }  
