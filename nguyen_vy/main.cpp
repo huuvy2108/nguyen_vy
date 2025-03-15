a2f3da3ecb85c5c329733f7ddae17452ee5148a0
huuvy2108 <huuvy2108@gmail.com>
2025-03-16 02:03:25 +0700

    Bài 7: Giá trị lớn nhất trong mảng

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -5,27 +5,12 @@ void main()
     int SLN = 0;
     int arr_1[] = { 1,2,3,4,5,6,7,8,9 };
     int size = sizeof(arr_1) / sizeof(arr_1[0]);
-    printf("Cac so nguyen to trong mang: ");
+	int max = arr_1[0];
     for (int i = 0; i < size; i++)
     {
-		int num = arr_1[i];
-		int SNT = 1;
-		if (num < 2)
-		{
-			SNT = 0;
-		}
-		else
-		{
-			for (int j = 2; j <= num / 2; j++)
-			{
-				if (num % j == 0)
-				{
-					SNT = 0;
-				}
-			}
-		}
-		if (SNT) {
-			printf("%d\n ", num);
+        if (arr_1[i] > max) {
+            max = arr_1[i];
 		}
     }
+    printf("so lon nhat trong mang: %d\n",max);
 }  
