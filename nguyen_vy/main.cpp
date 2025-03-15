f9a2fbaf6cbf27283d4e98a8da55d72d154fd242
huuvy2108 <huuvy2108@gmail.com>
2025-03-16 01:45:41 +0700

    Bài 5: In các số lẻ trong mảng

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -5,14 +5,12 @@ void main()
     int SLN = 0;
     int arr_1[] = { 1,2,-2,4,-4,3,1,3 };
     int size = sizeof(arr_1) / sizeof(arr_1[0]);
+    printf("Cac so le trong mang: ");
     for (int i = 0; i < size; i++)
     {
-        if (arr_1[i] < 0) {
-            arr_1[i] = 0;
+        if (arr_1[i] % 2 != 0) {
+            printf("%d ", arr_1[i]);
         }
     }
-    printf("Mang sau khi thay the so am bang 0:\n");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr_1[i]);
-    }
+    printf("\n");
 }  
