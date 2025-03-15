f8cb2530137fa504d5037bed63050eaf1db1f39a
huuvy2108 <huuvy2108@gmail.com>
2025-03-16 01:58:52 +0700

    Bài 6: Các giá trị là số nguyên tố trong mảng

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -3,14 +3,29 @@
 void main()
 {
     int SLN = 0;
-    int arr_1[] = { 1,2,-2,4,-4,3,1,3 };
+    int arr_1[] = { 1,2,3,4,5,6,7,8,9 };
     int size = sizeof(arr_1) / sizeof(arr_1[0]);
-    printf("Cac so le trong mang: ");
+    printf("Cac so nguyen to trong mang: ");
     for (int i = 0; i < size; i++)
     {
-        if (arr_1[i] % 2 != 0) {
-            printf("%d ", arr_1[i]);
-        }
+		int num = arr_1[i];
+		int SNT = 1;
+		if (num < 2)
+		{
+			SNT = 0;
+		}
+		else
+		{
+			for (int j = 2; j <= num / 2; j++)
+			{
+				if (num % j == 0)
+				{
+					SNT = 0;
+				}
+			}
+		}
+		if (SNT) {
+			printf("%d\n ", num);
+		}
     }
-    printf("\n");
 }  
