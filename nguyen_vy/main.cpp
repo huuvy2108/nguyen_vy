1544a6c5674c977e36715676b79147cf05294e6f
huuvy2108 <huuvy2108@gmail.com>
2025-03-18 01:57:12 +0700

    Bài 5: In hoa chữ cái đầu tiên

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -3,22 +3,24 @@
 void main()
 {
     char str_1[100];
-    int dem = 0, i = 0;
-	int tu = 0;
+    int  i = 0;
+	int  viethoa = 1;
     printf("Nhap chuoi: ");
     gets_s(str_1, sizeof(str_1));
 
     while (str_1[i] != '\0') {
-		if (str_1[i] != ' ' && str_1[i] != '\t') {
-			if (tu == 0) {
-				dem++;
-                tu = 1;
-			}
-            else {
-				tu = 0;
+        if (str_1[i] != ' ' && str_1[i] != '\t') 
+        {
+            if (viethoa == 1 && str_1[i] >= 'a' && str_1[i] <= 'z') 
+            {
+                str_1[i] = str_1[i] - ('a' - 'A');
             }
-		}
+            viethoa = 0;
+        }
+        else {
+			viethoa = 1;
+            }
         i++;
-    }
-    printf("So tu trong chuoi: %d\n", dem);
+	}
+	printf("Chuoi sau khi chuan hoa: %s\n", str_1);
 }
