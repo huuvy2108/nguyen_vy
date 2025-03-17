8867852150200af418ba25089f1d95d8ea4289e8
huuvy2108 <huuvy2108@gmail.com>
2025-03-18 01:24:07 +0700

    Bài 2: Đếm số ký tự Space trong chuỗi

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -3,11 +3,15 @@
 void main()
 {
     char str_1[100];
-    int dem = 0;
+	int dem = 0, i = 0;
     printf("Nhap chuoi: ");
     gets_s(str_1, sizeof(str_1));
-    while (str_1[dem] != '\0') {
-        dem++;
+
+    while (str_1[i] != '\0') {
+        if (str_1[i] == ' ') {
+			dem++;
+        }
+        i++;
     }
-    printf("So ky tu trong chuoi la: %d\n", dem);
+    printf("So ky tu space trong chuoi la: %d\n", dem);
 }
