d5020eb9bb2b34515daadd7d18e7e028541fe9ed
huuvy2108 <huuvy2108@gmail.com>
2025-03-18 01:46:23 +0700

    Bài 4: Đếm số từ trong chuỗi

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -3,15 +3,22 @@
 void main()
 {
     char str_1[100];
-	int i = 0;
+    int dem = 0, i = 0;
+	int tu = 0;
     printf("Nhap chuoi: ");
     gets_s(str_1, sizeof(str_1));
 
     while (str_1[i] != '\0') {
-        if (str_1[i] >= 'a' && str_1[i] <= 'z') {
-			str_1[i] = str_1[i] - ('a' - 'A');
+		if (str_1[i] != ' ' && str_1[i] != '\t') {
+			if (tu == 0) {
+				dem++;
+                tu = 1;
+			}
+            else {
+				tu = 0;
+            }
 		}
         i++;
     }
-    printf("Chuoi sau khi chuyen thanh in hoa: %s\n", str_1);
+    printf("So tu trong chuoi: %d\n", dem);
 }
