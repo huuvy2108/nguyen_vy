7fc95d79ed8da6505c9d5f8c61dd1791c612b079
huuvy2108 <huuvy2108@gmail.com>
2025-03-18 17:57:42 +0700

    Bài 6: Trả về ký tự c trong chuỗi

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -4,23 +4,24 @@ void main()
 {
     char str_1[100];
     int  i = 0;
-	int  viethoa = 1;
+    char c;
+	char* diachi = NULL;
     printf("Nhap chuoi: ");
     gets_s(str_1, sizeof(str_1));
 
+	printf("Nhap ky tu can tim: ");
+	scanf_s(" %c", &c, 1);
+
     while (str_1[i] != '\0') {
-        if (str_1[i] != ' ' && str_1[i] != '\t') 
-        {
-            if (viethoa == 1 && str_1[i] >= 'a' && str_1[i] <= 'z') 
-            {
-                str_1[i] = str_1[i] - ('a' - 'A');
-            }
-            viethoa = 0;
+        if (str_1[i] != c) {
+            diachi = &str_1[i];
         }
-        else {
-			viethoa = 1;
-            }
         i++;
+    }
+    if (diachi != NULL) {
+        printf("Ky tu '%c' duoc tim thay tai dia chi: %p\n ", c, diachi);
+    }
+    else {
+        printf("Ky tu '%c' khong co trong chuoi. \n", c);
 	}
-	printf("Chuoi sau khi chuan hoa: %s\n", str_1);
 }
