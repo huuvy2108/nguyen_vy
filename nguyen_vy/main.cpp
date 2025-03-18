470513f212d836ac37743fccd146fcfff42d24eb
huuvy2108 <huuvy2108@gmail.com>
2025-03-18 18:16:33 +0700

    Bài 7: Tìm chuỗi subStr trong chuỗi Str

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -2,26 +2,29 @@
 
 void main()
 {
-    char str_1[100];
-    int  i = 0;
-    char c;
-	char* diachi = NULL;
-    printf("Nhap chuoi: ");
+	char str_1[100], subStr[50];
+    int  i = 0, j;
+	char *ketqua = NULL;
+    printf("Nhap chuoi chinh: ");
     gets_s(str_1, sizeof(str_1));
 
-	printf("Nhap ky tu can tim: ");
-	scanf_s(" %c", &c, 1);
+	printf("Nhap chuoi can tim: ");
+	gets_s(subStr, sizeof(subStr));
 
     while (str_1[i] != '\0') {
-        if (str_1[i] != c) {
-            diachi = &str_1[i];
+        j = 0;
+		while (subStr[j] != '\0' && str_1[i + j] == subStr[j]) {
+			j++;
+		}
+        if (subStr[j] == '\0') {
+            ketqua = &str_1[i];
         }
         i++;
     }
-    if (diachi != NULL) {
-        printf("Ky tu '%c' duoc tim thay tai dia chi: %p\n ", c, diachi);
+    if (ketqua != NULL) {
+        printf("Chuoi \"%s\" duoc tim thay tai dia chi: %p\n ", subStr, ketqua);
     }
     else {
-        printf("Ky tu '%c' khong co trong chuoi. \n", c);
+        printf("Chuoi '%s' khong co trong chuoi. \n", subStr);
 	}
 }
