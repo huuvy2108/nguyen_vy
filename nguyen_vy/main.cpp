1119a5dd5a36b66326854029c8e83bc9bd0d0674
huuvy2108 <huuvy2108@gmail.com>
2025-03-20 00:09:40 +0700

    Bài 2: Dùng hàm tính trung bình số lẻ trong mảng

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,20 +1,30 @@
 ﻿#include <stdio.h>
 
-//Chuc nang: tinh tong
+//Chuc nang: tinh tb
 //input: Mang, so phan tu cua mang
 //ket qua: tong
-int tinhTong(int arr[], int size) {
-    int sum = 0;
+float tinhTrungBinhLe(int arr[], int size) {
+    int sum = 0, count = 0;
     for (int i = 0; i < size; i++) {
-        sum += arr[i];
+        if (arr[i] % 2 != 0) {
+            sum += arr[i];
+            count++;
+        }
     }
-    return sum;
+
+	return (count > 0) ? (float)sum / count : -1;
 }
 void main()
 {
     int arr_1[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     int size = sizeof(arr_1) / sizeof(arr_1[0]);
-    int sum = tinhTong(arr_1, size);
 
-    printf("Tong cac phan tu trong mang: %d\n", sum);
+    float average = tinhTrungBinhLe(arr_1, size);
+
+    if (average != -1) {
+        printf("Trung binh cong cac so LE trong mang: %.2f\n", average);
+    }
+    else {
+        printf("Khong co so le trong mang.\n");
+    }
 }
