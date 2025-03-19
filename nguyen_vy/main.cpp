f23376e24ea967bd332c9f04093d3faa0e8c79f0
huuvy2108 <huuvy2108@gmail.com>
2025-03-20 00:49:00 +0700

    Bài 1: Xây dụng hàm. dùng để điếm số ký tự trong chuỗi

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,41 +1,21 @@
 ﻿#include <stdio.h>
 
-// Chức năng: Hoán vị 2 số nguyên
-// input:số nguyên a, b
-// Kết quả trả về: sau khi hoán vị, a = b, b = a
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-// Chức năng: Hàm sắp xếp
-void sapxep(int arr[], int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(&arr[j], &arr[j + 1]);  // Gọi hàm hoán vị
-            }
-        }
+// Chức năng: Đếm số ký tự trong chuỗi
+// input: Địa chỉ phần tử đầu tiên của chuỗi
+// Kết quả trả về: Sô ký tự trong chuỗi
+int dem_ky_tu(char* str) {
+    int dem = 0;
+    while (str[dem] != '\0') {
+        dem++;
     }
+    return dem;
 }
 
-// Chức năng: Hàm in mảng
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
+void main() {
+    char str_1[100];
 
-int main() {
-    int arr_1[] = { 5, 2, 9, 1, 7, 6, 8, 3, 4, 0 };
-    int size = sizeof(arr_1) / sizeof(arr_1[0]);
+    printf("Nhap chuoi: ");
+    gets_s(str_1, sizeof(str_1)); // Hoặc thay bằng fgets(str_1, sizeof(str_1), stdin);
 
-    printf("Mang ban dau: ");
-    printArray(arr_1, size);
-    sapxep(arr_1, size);
-    printf("Mang sau khi sap xep tang dan: ");
-    printArray(arr_1, size);
-
+    printf("So ky tu trong chuoi la: %d\n", dem_ky_tu(str_1));
 }
