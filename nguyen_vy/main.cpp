1aec01f4094b84586cc85a49b07521e85dd761ef
huuvy2108 <huuvy2108@gmail.com>
2025-03-19 23:59:48 +0700

    Bài 1: Xây dựng hàm. tính tổng các giá trị trong mảng

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,30 +1,20 @@
 ﻿#include <stdio.h>
 
+//Chuc nang: tinh tong
+//input: Mang, so phan tu cua mang
+//ket qua: tong
+int tinhTong(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
 void main()
 {
-	char str_1[100], subStr[50];
-    int  i = 0, j;
-	char *ketqua = NULL;
-    printf("Nhap chuoi chinh: ");
-    gets_s(str_1, sizeof(str_1));
+    int arr_1[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int size = sizeof(arr_1) / sizeof(arr_1[0]);
+    int sum = tinhTong(arr_1, size);
 
-	printf("Nhap chuoi can tim: ");
-	gets_s(subStr, sizeof(subStr));
-
-    while (str_1[i] != '\0') {
-        j = 0;
-		while (subStr[j] != '\0' && str_1[i + j] == subStr[j]) {
-			j++;
-		}
-        if (subStr[j] == '\0') {
-            ketqua = &str_1[i];
-        }
-        i++;
-    }
-    if (ketqua != NULL) {
-        printf("Chuoi \"%s\" duoc tim thay tai dia chi: %p\n ", subStr, ketqua);
-    }
-    else {
-        printf("Chuoi '%s' khong co trong chuoi. \n", subStr);
-	}
+    printf("Tong cac phan tu trong mang: %d\n", sum);
 }
