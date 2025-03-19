07324da5ed65bc5c7cc1c1ef517e32e0e0034bde
huuvy2108 <huuvy2108@gmail.com>
2025-03-20 00:19:41 +0700

    Bài 6: Xây dựng hàm. in ra các giá trị là số nguyên tố trong mảng

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,21 +1,34 @@
 ﻿#include <stdio.h>
 
-//Chuc nang: in số lẻ trong mảng
-//input: Mang, so phan tu cua mang
-//ket qua: không có
-void inSoLe(int arr[], int size) {
-    printf("Cac so le trong mang: ");
+//Chức năng: Xác định SNT
+//input:Giá trị cần kiểm tra
+//Kết quả trả về: Là SNT: 1, Không là SNT: 0
+int laSoNguyenTo(int num) {
+    if (num < 2) {
+        return 0;
+    }
+    for (int j = 2; j * j <= num; j++) {
+        if (num % j == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// Chức năng: Hàm in các số nguyên tố trong mảng
+void inSoNguyenTo(int arr[], int size) {
+    printf("Cac so nguyen to trong mang: ");
     for (int i = 0; i < size; i++) {
-        if (arr[i] % 2 != 0) {
+        if (laSoNguyenTo(arr[i])) {
             printf("%d ", arr[i]);
         }
     }
     printf("\n");
 }
 
-void main() {
-    int arr_1[] = { 1, 2, -2, 4, -4, 3, 1, 3 };
+int main() {
+    int arr_1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     int size = sizeof(arr_1) / sizeof(arr_1[0]);
 
-    inSoLe(arr_1, size);
+    inSoNguyenTo(arr_1, size);
 }
