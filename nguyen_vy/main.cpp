9c773797c64d5f3d1e47e8dc99bf1d8c75b5b08d
huuvy2108 <huuvy2108@gmail.com>
2025-03-20 00:13:25 +0700

    Bài 5: Xây dụng hàm. in ra các giá trị LẼ trong mảng

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,30 +1,21 @@
 ﻿#include <stdio.h>
 
-//Chuc nang: tinh tb
+//Chuc nang: in số lẻ trong mảng
 //input: Mang, so phan tu cua mang
-//ket qua: tong
-float tinhTrungBinhLe(int arr[], int size) {
-    int sum = 0, count = 0;
+//ket qua: không có
+void inSoLe(int arr[], int size) {
+    printf("Cac so le trong mang: ");
     for (int i = 0; i < size; i++) {
         if (arr[i] % 2 != 0) {
-            sum += arr[i];
-            count++;
+            printf("%d ", arr[i]);
         }
     }
-
-	return (count > 0) ? (float)sum / count : -1;
+    printf("\n");
 }
-void main()
-{
-    int arr_1[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+void main() {
+    int arr_1[] = { 1, 2, -2, 4, -4, 3, 1, 3 };
     int size = sizeof(arr_1) / sizeof(arr_1[0]);
 
-    float average = tinhTrungBinhLe(arr_1, size);
-
-    if (average != -1) {
-        printf("Trung binh cong cac so LE trong mang: %.2f\n", average);
-    }
-    else {
-        printf("Khong co so le trong mang.\n");
-    }
+    inSoLe(arr_1, size);
 }
