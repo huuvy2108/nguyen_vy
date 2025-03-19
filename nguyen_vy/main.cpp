f00d64ddfc78af4c54c4c8e183484bef0c5c560f
huuvy2108 <huuvy2108@gmail.com>
2025-03-20 00:32:29 +0700

    Bài 9: Xây dựng hàm. Sắp xếp mảng theo giá trị tăng dần

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,24 +1,41 @@
 ﻿#include <stdio.h>
 
-//Chức năng: Hoán vị 2 số nguyên
-//input:số nguyên a, b
-//Kết quả trả về: sau khi hoán vị, a = b, b = a
-void swap(int* x, int* y) {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+// Chức năng: Hoán vị 2 số nguyên
+// input:số nguyên a, b
+// Kết quả trả về: sau khi hoán vị, a = b, b = a
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void main() {
-    int a, b;
-    printf("Nhap hai so nguyen: \n");
-
-    if (scanf_s("%d %d", &a, &b) != 2) {
-        printf("Loi: Vui long nhap hai so nguyen!\n");
+// Chức năng: Hàm sắp xếp
+void sapxep(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);  // Gọi hàm hoán vị
+            }
+        }
     }
+}
 
-    printf("Truoc khi hoan vi: a = %d, b = %d\n", a, b);
-    swap(&a, &b);
-    printf("Sau khi hoan vi: a = %d, b = %d\n", a, b);
+// Chức năng: Hàm in mảng
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr_1[] = { 5, 2, 9, 1, 7, 6, 8, 3, 4, 0 };
+    int size = sizeof(arr_1) / sizeof(arr_1[0]);
+
+    printf("Mang ban dau: ");
+    printArray(arr_1, size);
+    sapxep(arr_1, size);
+    printf("Mang sau khi sap xep tang dan: ");
+    printArray(arr_1, size);
 
 }
