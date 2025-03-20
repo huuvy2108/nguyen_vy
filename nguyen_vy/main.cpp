794430fcdafc2cc11e5c082c94ed3f74abd28f98
huuvy2108 <huuvy2108@gmail.com>
2025-03-20 18:30:13 +0700

    Bài 7: Xây dựng hàm, tìm chuỗi subStr trong chuỗi str, trả về địa chỉ bất đầu của subStr trong chuỗi str

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,35 +1,39 @@
 ﻿#include <stdio.h>
 
-// Chức năng: Tìm ký tự c trong chuỗi
-// input: Địa chỉ phần tử đầu tiên của chuỗi, ký tự cần tìm
-// Kết quả trả về: Địa chỉ của ký tự cần tìm thấy trong chuỗi
-char* tim_ky_tu(const char* str, char c) {
-    while (*str != '\0') {
-        if (*str == c) {
-            return (char*)str;
+// Chức năng: Tìm chuỗi substr trong chuỗi str
+// input: Địa chỉ phần tử đầu tiên của chuỗi, địa chỉ phần tử đầu tiên của chuỗi con cần tìm
+// Kết quả trả về: Địa chỉ đầu tiên của chuỗi con trong chuỗi, nếu không tìm thấy trả về NULL
+char* tim_chuoi_con(char* str, char* subStr) {
+    int i, j;
+
+    for (i = 0; str[i] != '\0'; i++) {
+        j = 0;
+        while (subStr[j] != '\0' && str[i + j] == subStr[j]) {
+            j++;
         }
-        str++;
+        if (subStr[j] == '\0') {
+            return (char*)&str[i];
+        }
     }
     return NULL;
 }
 
-int main() {
-    char str_1[100];
-    char c;
-    char* diachi;
+void main() {
+    char str_1[100], subStr[50];
+    char* ketqua;
 
-    printf("Nhap chuoi: ");
+    printf("Nhap chuoi chinh: ");
     gets_s(str_1, sizeof(str_1));
 
-    printf("Nhap ky tu can tim: ");
-    scanf_s(" %c", &c, 1);
+    printf("Nhap chuoi can tim: ");
+    gets_s(subStr, sizeof(subStr));
 
-    diachi = tim_ky_tu(str_1, c);
+    ketqua = tim_chuoi_con(str_1, subStr);
 
-    if (diachi != NULL) {
-        printf("Ky tu '%c' duoc tim thay tai dia chi: %p\n", c, diachi);
+    if (ketqua != NULL) {
+        printf("Chuoi \"%s\" duoc tim thay tai dia chi: %p\n", subStr, ketqua);
     }
     else {
-        printf("Ky tu '%c' khong co trong chuoi.\n", c);
+        printf("Chuoi \"%s\" khong co trong chuoi.\n", subStr);
     }
 }
