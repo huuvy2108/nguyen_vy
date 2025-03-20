db4e273ccb754ae3e5a7210389a297af018f5bf4
huuvy2108 <huuvy2108@gmail.com>
2025-03-20 17:51:08 +0700

    Bài 4: Xây dựng hàm, đếm số từ trong chuỗi

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,22 +1,30 @@
 ﻿#include <stdio.h>
 
-// Chức năng: Chuyển chữ thường thành chữ hoa
+// Chức năng: Đếm số từ trong chuỗi
 // input: Địa chỉ phần tử đầu tiên của chuỗi
-// Kết quả trả về: Chữ hoa của chuỗi
-void chuyen_hoa(char* str) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] >= 'a' && str[i] <= 'z') {
-            str[i] = str[i] - ('a' - 'A');
-        }
-    }
+// Kết quả trả về: Số từ trong chuỗi
+int dem_so_tu(char* str) {
+	int dem = 0, tu = 0;
+	for (int i = 0; str[i] != '\0'; i++) {
+		if (str[i] != ' ' && str[i] != '\t') {
+			if (tu == 0) {
+				dem++;
+				tu = 1;
+			}
+		}
+		else {
+			tu = 0;
+		}
+	}
+	return dem;
 }
 
 void main() {
-    char str_1[100];
+	char str_1[100];
 
-    printf("Nhap chuoi: ");
-    gets_s(str_1, sizeof(str_1));
+	printf("Nhap chuoi: ");
+	gets_s(str_1, sizeof(str_1));
 
-    chuyen_hoa(str_1);
-    printf("Chuoi sau khi chuyen thanh in hoa: %s\n", str_1);
+	int so_tu = dem_so_tu(str_1);
+	printf("So tu trong chuoi: %d\n", so_tu);
 }
