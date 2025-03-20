847a630c24b41f0bfd526f6cae76f77ab3c69326
huuvy2108 <huuvy2108@gmail.com>
2025-03-20 17:56:21 +0700

    Bài 5: Xây dụng hàm, in hoa chữ cái đầu tiên của từ trong chuỗi

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,30 +1,29 @@
 ﻿#include <stdio.h>
 
-// Chức năng: Đếm số từ trong chuỗi
+// Chức năng: Viết hoa chữ cái đầu mỗi từ trong chuỗi
 // input: Địa chỉ phần tử đầu tiên của chuỗi
-// Kết quả trả về: Số từ trong chuỗi
-int dem_so_tu(char* str) {
-	int dem = 0, tu = 0;
-	for (int i = 0; str[i] != '\0'; i++) {
-		if (str[i] != ' ' && str[i] != '\t') {
-			if (tu == 0) {
-				dem++;
-				tu = 1;
-			}
-		}
-		else {
-			tu = 0;
-		}
-	}
-	return dem;
+// Kết quả trả về: Chữ cái đầu mỗi từ trong chuỗi được viết hoa
+void chuan_hoa_chuoi(char* str) {
+    int viethoa = 1;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ' && str[i] != '\t') {
+            if (viethoa == 1 && str[i] >= 'a' && str[i] <= 'z') {
+                str[i] = str[i] - ('a' - 'A');
+            }
+            viethoa = 0;
+        }
+        else {
+            viethoa = 1;
+        }
+    }
 }
 
 void main() {
-	char str_1[100];
+    char str_1[100];
 
-	printf("Nhap chuoi: ");
-	gets_s(str_1, sizeof(str_1));
+    printf("Nhap chuoi: ");
+    gets_s(str_1, sizeof(str_1));
 
-	int so_tu = dem_so_tu(str_1);
-	printf("So tu trong chuoi: %d\n", so_tu);
+    chuan_hoa_chuoi(str_1);
+    printf("Chuoi sau khi chuan hoa: %s\n", str_1);
 }
