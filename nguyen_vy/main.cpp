ed61d4e3a6b7de09be041988fa1c582ea8bea58a
huuvy2108 <huuvy2108@gmail.com>
2025-03-27 03:30:43 +0700

    Bài 6. Struct: Rút gọn phân số

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -5,20 +5,28 @@ typedef struct {
 	int mau;
 }phan_so_t;
 
-// Chức năng: Trừ 2 phân số
+int UCLN(int a, int b) {
+	if (a == 0 || b == 0) return a + b;
+	while (a != b) {
+		if (a > b) a -= b;
+		else b -= a;
+	}
+	return a;
+}
+
+// Chức năng: Rút gọn phân số
 // input: 
-// Phân số thứ nhất
-// Phân số thứ hai
-// Output: Kết quả phép tính
-phan_so_t Tru_phan_so(phan_so_t x, phan_so_t y) {
-	phan_so_t kq;
-	kq.tu = x.tu * y.mau - y.tu * x.mau;
-	kq.mau = x.mau * y.mau;
-	return kq;
+// Phân số
+// Output: Kết quả phân số sau khi rút gọn
+phan_so_t Rut_gon_phan_so(phan_so_t x) {
+	int ucln = UCLN(x.tu, x.mau);
+	x.tu /= ucln;
+	x.mau /= ucln;
+	return x;
 }
 
 void main() {
-	phan_so_t x = { 1, 2 }, y = { 3, 4 };
-	phan_so_t kq = Tru_phan_so(x, y);
+	phan_so_t x = { 6, 10 };
+	phan_so_t kq = Rut_gon_phan_so(x);
 	printf("%d/%d\n", kq.tu, kq.mau);
 }
