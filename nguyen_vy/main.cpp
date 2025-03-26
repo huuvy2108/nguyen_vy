3d3e354b72e9851c01727dc7a63fb210275945f2
huuvy2108 <huuvy2108@gmail.com>
2025-03-27 03:17:18 +0700

    Bài 4. Struct: Cộng hai phân số

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -5,20 +5,20 @@ typedef struct {
 	int mau;
 }phan_so_t;
 
-// Chức năng: Nhân 2 phân số
+// Chức năng: Cộng 2 phân số
 // input: 
 // Phân số thứ nhất
 // Phân số thứ hai
 // Output: Kết quả phép tính
-phan_so_t Chia_phan_so(phan_so_t x, phan_so_t y) {
+phan_so_t Cong_phan_so(phan_so_t x, phan_so_t y) {
 	phan_so_t kq;
-	kq.tu = x.tu * y.mau;
-	kq.mau = x.mau * y.tu;
+	kq.tu = x.tu * y.mau + y.tu * x.mau;
+	kq.mau = x.mau * y.mau;
 	return kq;
 }
 
 void main() {
 	phan_so_t x = { 1, 2 }, y = { 3, 4 };
-	phan_so_t kq = Chia_phan_so(x, y);
+	phan_so_t kq = Cong_phan_so(x, y);
 	printf("%d/%d\n", kq.tu, kq.mau);
 }
