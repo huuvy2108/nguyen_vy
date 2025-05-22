66853c5c52c2fad7d6f152cf95fdff4194ba7a87
huuvy2108 <huuvy2108@gmail.com>
2025-05-22 23:12:54 +0700

    Bài 4: Viết hàm xử lý để lấy trạng thái của thiết bị

--- nguyen_vy/main.cpp
+++ nguyen_vy/main.cpp
@@ -1,53 +1,44 @@
-﻿#include <iostream>
+﻿#include <stdio.h>
+#include <string.h>
 
-class Phuongtrinhbac2 {
-private:
-	double a, b, c; 
-public:
-	Phuongtrinhbac2() : a(0), b(0), c(0) {}
-	void Nhap() {
-		std::cout << "Nhap he so a: ";
-		std::cin >> a;
-		std::cout << "Nhap he so b: ";
-		std::cin >> b;
-		std::cout << "Nhap he so c: ";
-		std::cin >> c;
-	}
+typedef struct {
+    char light;
+    char fan;
+    char motor;
+} smartHome_t;
 
-	void Giaipt() {
-		if (a == 0) {
-			if (b == 0) {
-				if (c == 0) {
-					std::cout << "Phuong trinh co vo so nghiem." << std::endl;
-				}
-				else {
-					std::cout << "Phuong trinh vo nghiem." << std::endl;
-				}
-			}
-			else {
-				std::cout << "Phuong trinh co 1 nghiem: x = " << -c / b << std::endl;
-			}
+const char* data = "HTTP1.1 200 OK{"
+"\"light\": \"on\","
+"\"fan\" : \"off\","
+"\"motor\" : \"off\"}";
+
+char getStatus(const char* data, const char* key) {
+	const char* pos = strstr(data, key);
+	if (pos != NULL) {
+		const char* on_pos = strstr(pos, "on");
+		const char* off_pos = strstr(pos, "off");
+		if (on_pos != NULL && on_pos < off_pos) {
+			return 1;
 		}
-		else {
-			double delta = b * b - 4 * a * c;
-			if (delta > 0) {
-				double x1 = (-b + sqrt(delta)) / (2 * a);
-				double x2 = (-b - sqrt(delta)) / (2 * a);
-				std::cout << "Phuong trinh co 2 nghiem phan biet: x1 = " << x1 << ", x2 = " << x2 << std::endl;
-			}
-			else if (delta == 0) {
-				double x = -b / (2 * a);
-				std::cout << "Phuong trinh co nghiem kep: x = " << x << std::endl;
-			}
-			else {
-				std::cout << "Phuong trinh vo nghiem." << std::endl;
-			}
+		else if (off_pos != NULL) {
+			return 0;
 		}
 	}
-};
+	return 0;
+}
 
-void main() {
-	Phuongtrinhbac2 pt;
-	pt.Nhap();
-	pt.Giaipt();
+smartHome_t pair_data(const char* data) {
+	smartHome_t result;
+	result.light = getStatus(data, "\"light\"");
+	result.fan = getStatus(data, "\"fan\"");
+	result.motor = getStatus(data, "\"motor\"");
+	return result;
+}
+
+int main() {
+	smartHome_t home = pair_data(data);
+	printf("Light: %d\n", home.light);
+	printf("Fan: %d\n", home.fan);
+	printf("Motor: %d\n", home.motor);
+	return 0;
 }
